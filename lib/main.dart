682ef518c762e055dba87c237b9a6450ed5b0084
Ömer Kadir <omerkdr8@outlook.com>
2024-02-27 21:01:50 +0300

    controllers added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,6 +29,21 @@ class AudioPlayerScreen extends StatefulWidget {
 }
 
 class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
+  late AudioPlayer _audioPlayer;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer()..setAsset('assets/audio/369hz.MP3');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,6 +80,44 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
         ),
       ),
+    );
+  }
+}
+
+class Controls extends StatelessWidget {
+  const Controls({required this.audioPlayer, super.key});
+  final AudioPlayer audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<PlayerState>(
+      stream: audioPlayer.playerStateStream,
+      builder: (context, snapshot) {
+        final playerState = snapshot.data;
+        final proccesingState = playerState?.processingState;
+        final playing = playerState?.playing;
+        if (!(playing ?? false)) {
+          return IconButton(
+            onPressed: audioPlayer.play,
+            iconSize: 75,
+            color: Colors.white,
+            icon: const Icon(Icons.play_arrow_rounded),
+          );
+        } else if (proccesingState != ProcessingState.completed) {
+          return IconButton(
+            onPressed: audioPlayer.pause,
+            iconSize: 75,
+            color: Colors.white,
+            icon: const Icon(Icons.pause_rounded),
+          );
+        }
+        return IconButton(
+          onPressed: () => audioPlayer.seek(Duration.zero),
+          iconSize: 75,
+          color: Colors.white,
+          icon: const Icon(Icons.replay_rounded),
+        );
+      },
     );
   }
 }
