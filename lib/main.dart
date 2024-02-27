1f13d24188310c4a853197c93989e1303499a8f4
Ömer Kadir <omerkdr8@outlook.com>
2024-02-28 01:17:23 +0300

    background play added

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,12 @@ import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:rxdart/rxdart.dart';
 
-void main() {
+Future<void> main() async {
+  await JustAudioBackground.init(
+    androidNotificationChannelName: 'Spiroot',
+    androidNotificationChannelDescription: 'Spiroot',
+    androidNotificationOngoing: true,
+  );
   runApp(const MyApp());
 }
 
@@ -54,7 +59,7 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
         Uri.parse('asset:///assets/audio/396hz.mp3'),
         tag: MediaItem(
           id: '0',
-          title: 'İyileştirici',
+          title: '396 hz',
           artist: 'Spiroot',
           artUri: Uri.parse(
             'https://images.unsplash.com/photo-1554832307-e5e32d164958?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -65,10 +70,10 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
         Uri.parse('asset:///assets/audio/639hz.mp3'),
         tag: MediaItem(
           id: '1',
-          title: 'İyileştirici',
+          title: '639 hz',
           artist: 'Spiroot',
           artUri: Uri.parse(
-            'https://images.unsplash.com/photo-1554832307-e5e32d164958?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1503919275948-1f118d8ecf0b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c3Bpcml0dWFsfGVufDB8fDB8fHww',
           ),
         ),
       ),
@@ -76,10 +81,10 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
         Uri.parse('asset:///assets/audio/852hz.mp3'),
         tag: MediaItem(
           id: '2',
-          title: 'İyileştirici',
+          title: '852 hz',
           artist: 'Spiroot',
           artUri: Uri.parse(
-            'https://images.unsplash.com/photo-1554832307-e5e32d164958?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1538024333176-f25f63f873ee?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
           ),
         ),
       ),
@@ -127,11 +132,13 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
         leading: IconButton(
           onPressed: () {},
           icon: const Icon(Icons.keyboard_arrow_down_rounded),
+          color: Colors.white,
         ),
         actions: [
           IconButton(
             onPressed: () {},
             icon: const Icon(Icons.more_horiz),
+            color: Colors.white,
           ),
         ],
       ),
@@ -268,35 +275,52 @@ class Controls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<PlayerState>(
-      stream: audioPlayer.playerStateStream,
-      builder: (context, snapshot) {
-        final playerState = snapshot.data;
-        final proccesingState = playerState?.processingState;
-        final playing = playerState?.playing;
-        if (!(playing ?? false)) {
-          return IconButton(
-            onPressed: audioPlayer.play,
-            iconSize: 75,
-            color: Colors.white,
-            icon: const Icon(Icons.play_arrow_rounded),
-          );
-        } else if (proccesingState != ProcessingState.completed) {
-          return IconButton(
-            onPressed: audioPlayer.pause,
-            iconSize: 75,
-            color: Colors.white,
-            icon: const Icon(Icons.pause_rounded),
-          );
-        }
-
-        return IconButton(
-          onPressed: () => audioPlayer.seek(Duration.zero),
-          iconSize: 75,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: audioPlayer.seekToPrevious,
+          iconSize: 60,
           color: Colors.white,
-          icon: const Icon(Icons.replay_rounded),
-        );
-      },
+          icon: const Icon(Icons.skip_previous_rounded),
+        ),
+        StreamBuilder<PlayerState>(
+          stream: audioPlayer.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            final proccesingState = playerState?.processingState;
+            final playing = playerState?.playing;
+            if (!(playing ?? false)) {
+              return IconButton(
+                onPressed: audioPlayer.play,
+                iconSize: 75,
+                color: Colors.white,
+                icon: const Icon(Icons.play_arrow_rounded),
+              );
+            } else if (proccesingState != ProcessingState.completed) {
+              return IconButton(
+                onPressed: audioPlayer.pause,
+                iconSize: 75,
+                color: Colors.white,
+                icon: const Icon(Icons.pause_rounded),
+              );
+            }
+
+            return IconButton(
+              onPressed: () => audioPlayer.seek(Duration.zero),
+              iconSize: 75,
+              color: Colors.white,
+              icon: const Icon(Icons.replay_rounded),
+            );
+          },
+        ),
+        IconButton(
+          onPressed: audioPlayer.seekToNext,
+          iconSize: 60,
+          color: Colors.white,
+          icon: const Icon(Icons.skip_next_rounded),
+        ),
+      ],
     );
   }
 }
