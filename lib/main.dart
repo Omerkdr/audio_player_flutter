2996052c0b806960944a7807be48af1ba7cff64c
Ömer Kadir <omerkdr8@outlook.com>
2024-03-04 23:54:39 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Audio',
+      title: 'Flutter Audio App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
